478cb42d996da82d0a6647efa747b7e0978682fd
“Will <wwh22@uw.edu>
2024-04-17 14:45:33 -0700

    Make footer background white

--- .vitepress/theme/components/Footer.vue
+++ .vitepress/theme/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="w-full p-4 text-center text-gray-400 text-sm absolute bottom-0 left-0">
+    <footer class="w-full p-4 bg-white text-center text-gray-400 text-sm absolute bottom-0 left-0">
         <p>
             Copyright © Jesse Bloom 2024-Present
         </p>
