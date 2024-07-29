5150e772b5eb85b399922e423dd7a005000788ea
jbloom <jbloom@fredhutch.org>
2024-07-29 11:02:54 -0700

    remove the "join the team" at top of team

--- .vitepress/theme/Team.vue
+++ .vitepress/theme/Team.vue
@@ -48,9 +48,6 @@ export default {
                 class="text-3xl leading-9 font-bold text-gray-800 tracking-tight sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
                 Meet the Team
             </h1>
-            <p class="text-lg leading-7 text-gray-600">
-                Get <a href="/about">in touch with us</a> if you're interested in joining the team!
-            </p>
         </div>
         <div class="w-full border-t border-gray-200"></div>
         <div v-for="category in categories" :key="category" class="category-section my-6">
