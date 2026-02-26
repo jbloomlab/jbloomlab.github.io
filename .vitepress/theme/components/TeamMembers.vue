2c70791224991a8cbda1e7b52fdbe4cce7472f3a
Will Perlichek <wperlich@cs.washington.edu>
2026-02-25 20:03:58 -0800

    add: Will as Master's Student

--- .vitepress/theme/components/TeamMembers.vue
+++ .vitepress/theme/components/TeamMembers.vue
@@ -1,7 +1,6 @@
 <script>
 import { computed } from 'vue'
 import TeamMembersItem from './TeamMembersItem.vue'
-
 export default {
     components: {
         TeamMembersItem
@@ -19,14 +18,13 @@ export default {
     }
 }
 </script>
-
 <template>
     <div :class="classes">
-        <div class="mx-auto max-w-sm grid grid-cols-1 md:grid-cols-[repeat(auto-fit,minmax(14rem,1fr))] gap-6" :class="{
-        'md:max-w-7xl': members.length >= 3,
-        'md:max-w-sm': members.length === 1,
-        'md:max-w-3xl': members.length === 2
-    }">
+        <div class="max-w-sm grid grid-cols-1 md:grid-cols-[repeat(auto-fit,minmax(14rem,1fr))] gap-6" :class="{
+            'md:max-w-7xl': members.length >= 3,
+            'md:max-w-[14rem]': members.length === 1,
+            'md:max-w-3xl': members.length === 2
+        }">
             <div v-for="member in members" :key="member.name" class="item">
                 <TeamMembersItem :member="member" />
             </div>
